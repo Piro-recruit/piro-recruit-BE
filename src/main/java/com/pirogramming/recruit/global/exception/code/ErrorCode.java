22ada67f593731560fffa6e496198660aeba18cf
Kimgyuilli <rlarbdlf222@gmail.com>
2025-07-21 01:02:33 +0900

    fix: error code 오류 수정

--- src/main/java/com/pirogramming/recruit/global/exception/code/ErrorCode.java
+++ src/main/java/com/pirogramming/recruit/global/exception/code/ErrorCode.java
@@ -15,7 +15,7 @@ public enum ErrorCode {
 	OAUTH_AUTH_FAILED(1005, "OAuth 인증에 실패했습니다."),
 
 	// --- 도메인 기반 상세 오류 ---
-	MEMBER_NOT_FOUND(2001, "해당 사용자를 찾을 수 없습니다."),
+	MEMBER_NOT_FOUND(2001, "해당 사용자를 찾을 수 없습니다.");
 
 
 	private final int code;
