ff6f3ec8391569f82bdcd71ee2f8334e3c265523
songmin0111 <violet2111@naver.com>
2025-09-01 01:56:14 +0900

    feat AwsSes 설정 추가

---
+++ src/main/java/com/pirogramming/recruit/global/config/AwsSesConfig.java
@@ -0,0 +1,37 @@
+package com.pirogramming.recruit.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class AwsSesConfig {
+
+    @Value("${aws.access-key}")
+    private String accessKey;
+
+    @Value("${aws.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public SesClient sesClient() {
+        log.info("AWS SES 클라이언트 초기화 - region: {}", region);
+
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return SesClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .build();
+    }
+}
